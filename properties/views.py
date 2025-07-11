9771c6e102e82cfa157a61e8cbc3de633ba9bb07
Brenda Jematia Bonareri <86654766+Bonbonjb@users.noreply.github.com>
2025-07-11 16:53:02 +0300

    Update views.py

--- properties/views.py
+++ properties/views.py
@@ -6,4 +6,4 @@ from .models import Property
 @cache_page(60 * 15)  # cache for 15 minutes
 def property_list(request):
     properties = Property.objects.all().values()
-    return JsonResponse(list(properties), safe=False)
+    return JsonResponse({"data": list(properties)})
